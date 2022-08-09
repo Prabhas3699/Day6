271cd2b9375fb09d4e4e248b2ffc0232f0ef35f1
Prabhas3699 <prabhak620@gmail.com>
2022-08-09 21:37:52 +0530

    program for reverse number

---
+++ src/ReverseNumber.java
@@ -0,0 +1,20 @@
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		
+		//initiating variables
+		int num = 123;
+		int rev=0;
+		int i;
+		
+		//using while function to iterate multiple values
+		while(num>0) {
+			i=num%10;
+			rev=rev*10+num%10;
+			num=num/10;
+		}
+		System.out.println(rev);
+	}
+
+}
